115ce90e6c8cd6fea5c34a335eee34fdce9dbf3e
Steven Marshall <SMarshall@chef.io>
2020-08-26 23:27:50 +0100

    [grep] Update to 3.4
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- plan.sh
+++ plan.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2209
 pkg_name=grep
 pkg_origin=core
-pkg_version=3.3
+pkg_version=3.4
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 Grep searches one or more input files for lines containing a match to a \
@@ -11,7 +11,7 @@ specified pattern. By default, Grep outputs the matching lines.\
 pkg_upstream_url="https://www.gnu.org/software/grep/"
 pkg_license=('GPL-3.0-or-later')
 pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz"
-pkg_shasum="b960541c499619efd6afe1fa795402e4733c8e11ebf9fafccc0bb4bccdc5b514"
+pkg_shasum="58e6751c41a7c25bfc6e9363a41786cff3ba5709cf11d5ad903cf7cce31cc3fb"
 pkg_deps=(
   core/glibc
   core/pcre
